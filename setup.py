a39d12bc21007f8e23dabb6a99537aca43da8679
Daniel Mattox <dmattox100@gmail.com>
2021-03-30 11:16:43 -0400

    Edited setup.py to reflect changes from original plugin

--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "filamentsensorsimplified"
+plugin_identifier = "filamentsensorusb"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_filamentsensorsimplified"
+plugin_package = "octoprint_filamentsensorusb"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Filament sensor simplified"
+plugin_name = "Filament sensor USB"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.2.3"
+plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Simple plugin reacting to filament sensor, issuing filament change gcode to printer on filament runout"""
+plugin_description = """Simple plugin reacting to filament sensor, issuing filament change gcode to printer on filament runout, adapted to use a USB serial interfaced sensor"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Lukáš Malatinský"
+plugin_author = "Daniel Mattox, forked from Lukáš Malatinský"
 
 # The plugin's author's mail address.
-plugin_author_email = "luckyx182@gmail.com"
+plugin_author_email = "dmattox10@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/luckyx182/Filament_sensor_simplified"
+plugin_url = "https://github.com/dmattox10/Filament_sensor_USB"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
